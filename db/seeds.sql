f108e9322348be3c04d5d659db214f5180f7e8e6
SlemJosh <joshua.slem@gmail.com>
2023-11-14 09:54:56 -0700

    Added some initial values to the seeds.sql file.

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+insert into department( id,, dept_name)
+values 
+
+insert into role( id, title, salary, department_id)
+values
+
+
+insert into employee(id, first_name, last_name, role_id, manager_id)
+values
