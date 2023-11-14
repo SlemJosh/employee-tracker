9e49c03bd4c0634bcc3f731e13a6e0f19ff30527
SlemJosh <joshua.slem@gmail.com>
2023-11-14 09:34:26 -0700

    Creating initial fields on the schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+-- Create or Even Delete the initial database
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+-- Tables in the Database
+CREATE TABLE departments();
+CREATE TABLE roles();
+CREATE TABLE employees();
