f446019b0be4c04ba8e5fb31b12f50126c4c205e
SlemJosh <joshua.slem@gmail.com>
2023-11-14 09:49:36 -0700

    Added the values for the tables as well as set the primary keys.

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,40 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 -- Tables in the Database
-CREATE TABLE departments();
-CREATE TABLE roles();
-CREATE TABLE employees();
+
+-- departments
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+
+);
+
+-- roles
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary INT NOT NULL, 
+    department_id INT, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+
+);
+
+-- employee
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT, 
+    manager_id INT NOT NULL, 
+    PRIMARY KEY(id), 
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE, 
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
